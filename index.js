011d78ce6a81ab49e50f39bd61e94b40facbda09
seonghoon07 <chltjdgns1009@gmail.com>
2024-10-17 00:28:25 +0900

    ADD: locahost:3000 CORS 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
